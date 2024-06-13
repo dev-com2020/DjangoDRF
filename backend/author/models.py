65b75a3fa88a3895cb6a78e799f767dc600b4f26
DEV-COM <kwp@tlen.pl>
2024-06-13 10:01:48 +0200

    13.06 10:01

--- backend/author/models.py
+++ backend/author/models.py
@@ -4,7 +4,15 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    bio = models.TextField()
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+
+class BlogAuthor(Author):
+    class Meta:
+        proxy = True
+
+    def perform_something(self):
+        pass
--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
+from author.models import Author
+
+
+class BaseTimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey("author.Author", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(Author, related_name='author_blogs', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
+
+
+class CoverImage(BaseTimeStampModel):
+    image_link = models.URLField()
+    blog = models.OneToOneField(Blog, related_name='blog_ci', on_delete=models.PROTECT)
