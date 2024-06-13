4c5055704caa3cc59dc5dd9474d72ea93f2782e4
DEV-COM <kwp@tlen.pl>
2024-06-13 12:23:48 +0200

    13.06 12:23

--- backend/author/admin.py
+++ backend/author/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from author import models
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Author, AuthorAdmin)
--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog import models
+
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Blog, BlogAdmin)
