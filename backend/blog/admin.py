055363d71a07777fa8a3ddff8030ed1d253f5aab
DEV-COM <kwp@tlen.pl>
2024-06-13 12:46:50 +0200

    13.06 12:46

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -4,7 +4,13 @@ from blog import models
 
 
 class BlogAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['title']
+    show_full_result_count = True
+    list_filter = ['title']
+    list_display = ['title', 'author', 'word_count']
+
+    def word_count(self, obj):
+        return len(obj.content.split())
 
 
 admin.site.register(models.Blog, BlogAdmin)
